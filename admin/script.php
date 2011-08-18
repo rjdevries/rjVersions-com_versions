f29b7a3cf065f24b3b67b79b0fc099449a32aff6
R.J. de Vries <post@rjdevries.eu>
2011-08-18 16:57:52 +0200

    Changes last couple of days

--- admin/script.php
+++ admin/script.php
@@ -5,23 +5,21 @@ defined('_JEXEC') or die('Restricted access');
 /**
  * Script file of Versions component
  */
-//class com_versionsInstallerScript
-//{
-//	/**
-//	 * method to run after an install/update/uninstall method
-//	 *
-//	 * @return void
-//	 */
-//	function postflight($type, $parent)
-//	{
-//		// $parent is the class calling this method
-//		// $type is the type of change (install, update or discover_install)
-//		echo "hallo";
-//		die();
-//		$db=JFactory::getDbo();
-//		$db->setQuery("DELETE FROM `#__menu` WHERE `title` = 'versions'");
-//		$db->query();
-//		echo '<p>' . JText::_('COM_VERSIONS_POSTFLIGHT_' . $type . '_TEXT') . '</p>';
-//	}
-//
-//}
+class com_versionsInstallerScript
+{
+	/**
+	 * method to run after an install/update/uninstall method
+	 */
+	function postflight($type, $parent){
+        if ($type == 'install' || $type == 'update') {
+            $db = JFactory::getDBO();
+            $query = $db->getQuery(true);
+            $query->delete('#__menu');
+            $id = JComponentHelper::getComponent('com_versions')->id;
+            $query->where('component_id = '.$id);
+            $query->where('client_id = 1');
+            $db->setQuery($query);
+            $db->query();
+        }   
+    }   
+}
